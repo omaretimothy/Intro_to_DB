92200028f1f8ce5ed97dddf522bcb0572a306d44
omaretimothy <omaretimothy53@gmail.com>
2025-07-07 15:50:10 +0300

    Update the insert statement

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-insert into customer (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 values (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
